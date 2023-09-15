c239fcdc62e9a7e33334098f7fe38f55cf980084
tomas <tomasnavatta@gmail.com>
2023-09-15 15:08:51 -0300

    nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/Navbar/NavBar';
+import NavBar from './components/Navbar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 function App() {
   return (
