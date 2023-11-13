5c2182be36be1a5c34eb794f9d7dd3514cc6de3a
tomas <tomasnavatta@gmail.com>
2023-11-13 11:51:10 -0300

    formulario finalizar compra con firebase

--- src/Cart/FinalizarCompra.jsx
+++ src/Cart/FinalizarCompra.jsx
@@ -1,22 +1,71 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { cartContext } from "../context/CartContext";
 import '../components/stylesshets/finalizarCompra.css'
-
+import Formulario from "../components/form/formulario";
+import { getFirestore, doc, getDoc, collection, getDocs, addDoc } from 'firebase/firestore'
 
 const FinalizarCompra =() => {
   const {cart} = useContext(cartContext)
+   const [order, setOrder] = useState("");
+   const [nameOrder, setNameOrder ] = useState()
+   const [phoneOrder, setPhoneOrder ] = useState()
+   const [mailOrder, setMailOrder ] = useState()
+
+   const totalOrder = cart.reduce((previous, current) => previous + current.amount * current.price, 0)
+
+
+
+   const formsOrderName = (e) => {
+    setNameOrder(e.target.value)
+   
+  };
+  
+  const formsOrderPhone = (e) => {
+    setPhoneOrder(e.target.value)
+   
+  };
+  
+  const formsOrderMail = (e) => {
+    setMailOrder(e.target.value)
+   
+  };
+
+
+  const sendOrder = () => {
+    const db = getFirestore()
+
+    const orderCollection = collection(db, "orders")
+    const orderData = {
+      buyer:{
+        nombre: nameOrder,
+        phone: phoneOrder,
+        mail:mailOrder
+
+      },
+
+     item:[...cart],
+     total: totalOrder
+  
+
+    }
+    addDoc(orderCollection, orderData).then(({id}) => {
+      setOrder(id)
+      console.log(order)
+    })
+    
+  }
   return (
     <>
     {cart.length > 0 ?
       ( <div className="centerr">
-      <div className="card">
+      <div className="cards">
         
         {cart.map((product) => (
           <div key={product.id} className="product-container">
             <img className="imgFinalizarCompra" src={product.image} alt={product.title} />
             <div className="product-details">
               <p>{product.title}</p>
-              <p>${product.price}</p>
+              <p>${product.amount * product.price}</p>
               <p>{product.amount}</p>
             </div>
           </div>
@@ -28,7 +77,43 @@ const FinalizarCompra =() => {
     </div>)
     
     }
+  <form className="formulario" >
+      <label className='label' htmlFor="nombre">Nombre:</label>
+      <input className='input'
+        type="text"
+        id="nombre"
+        name="nombre"
+        onChange={formsOrderName}
+      
+      />
+
+      <label className='label' htmlFor="telefono">Teléfono:</label>
+      <input className='input'
+        type="tel"
+        id="telefono"
+        name="telefono"
+        onChange={formsOrderPhone}
+        
+      />
+
+      <label className='label' htmlFor="correo">Correo electrónico:</label>
+      <input className='input'
+        type="email"
+        id="correo"
+        name="correo"
+        onChange={formsOrderMail}
+      
+      />
+
+
+    </form>
+    <div className="buttonComprar">
+    <button className="comprar" onClick={sendOrder}>Comprar</button>
+
+    </div>
+   
     </>
+   
 
   );
 };
